9f3ea977f8fe1fe762bcc577f2dd7b167eaa2333
Muhammad Nur Fadli <muhammadnurf10@gmail.com>
2023-08-09 17:38:46 +0700

    change style code

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 const express = require('express')
 const router = require('./router/routes')
 const app = express()
-const PORT = 4000
-
-app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `)
-})
-
 
 module.exports = router.apply(app)
