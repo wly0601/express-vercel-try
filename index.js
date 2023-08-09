d08d9e06bac715276ffdc15451c3361f2a8a9a22
Muhammad Nur Fadli <muhammadnurf10@gmail.com>
2023-08-09 18:00:58 +0700

    change style code

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require('express')
 const router = require('./router/routes')
 const app = express()
+const port = 8080;
+
+app.listen(port, () => {
+    console.log(`Server listening on port : ${port}`)
+    console.log(`Server on at ${Date(Date.now)}`);
+})
 
 module.exports = router.apply(app)
